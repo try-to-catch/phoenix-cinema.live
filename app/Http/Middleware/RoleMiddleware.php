f2f060565ecd2d3bef9ea7e2f6644be5274173c6
Kostya <koooooostaaa@gmail.com>
2023-07-21 20:43:34 +0300

    create role middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, string $roles): Response
+    {
+        if (!auth()->check()) {
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        $parsedRoles = explode(',', $roles);
+
+        if (!auth()->user()->roles()->whereIn('name', $parsedRoles)->exists()) {
+            abort(403, "You don't have enough permission to access this resource");
+        }
+        return $next($request);
+    }
+}
