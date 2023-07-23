6e45fb69c906cbd9afe075cbe5b8d57682ca3994
Kostya <koooooostaaa@gmail.com>
2023-07-23 22:42:03 +0300

    refactor two lines into one, change param name

--- app/Actions/Images/StoreThumbnailAction.php
+++ app/Actions/Images/StoreThumbnailAction.php
@@ -9,14 +9,13 @@ use Intervention\Image\Facades\Image;
 
 class StoreThumbnailAction
 {
-    public function handle(UploadedFile|File $thumbnail, string $path, int $width = 320, int $height = 472): string
+    public function handle(UploadedFile|File $thumbnail, string $directory, int $width = 320, int $height = 472): string
     {
-        $filePath = "images/$path/";
-        $fullPath = $filePath . $thumbnail->hashName();
+        $path = "images/$directory/" . $thumbnail->hashName();
 
         $image = Image::make($thumbnail)->fit($width, $height)->encode();
-        Storage::disk('public')->put($fullPath, $image);
+        Storage::disk('public')->put($path, $image);
 
-        return $fullPath;
+        return $path;
     }
 }
