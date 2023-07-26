73c72f73f5379a6be3844a9686ac817494950fa7
Kostya <koooooostaaa@gmail.com>
2023-07-26 21:32:49 +0300

    create StoreSeats action

---
+++ app/Actions/Seat/StoreSeatsAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Actions\Seat;
+
+use App\Models\Hall;
+
+/**
+ * This action is used to store seats in the hall in the correct format.
+ */
+class StoreSeatsAction
+{
+    public function handle(Hall $hall, array $seats): void
+    {
+        $preparedSeats = [];
+
+        foreach ($seats as $rowIndex => $row) {
+            foreach ($row as $seatIndex => $seatData) {
+                $seat = [
+                    'seat_number' => $seatIndex + 1,
+                    'row_number' => $rowIndex + 1,
+                    'type' => $seatData['type'],
+                ];
+
+                $preparedSeats[] = $seat;
+            }
+        }
+
+        $hall->seats()->createMany($preparedSeats);
+    }
+}
