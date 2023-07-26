c31c608e73072a6135eaefe5ec9e98bd74469387
Kostya <koooooostaaa@gmail.com>
2023-07-26 21:39:44 +0300

    register new resource routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\HallController;
 use App\Http\Controllers\Admin\MovieController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource('movies', MovieController::class);
+Route::resource('halls', HallController::class);
