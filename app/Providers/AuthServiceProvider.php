401d3dbdced7c863c577a61d6964097de2018c0b
Kostya <koooooostaaa@gmail.com>
2023-07-28 19:10:46 +0300

    register a new policy for the Hall model

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Hall;
+use App\Policies\Admin\HallPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Hall::class => HallPolicy::class,
     ];
 
     /**
