dbcf24101ca7edd274bc5f3828d12090038ef995
Kostya <koooooostaaa@gmail.com>
2023-07-28 19:00:49 +0300

    create UpdateHall FormRequest

---
+++ app/Http/Requests/Admin/Hall/UpdateHallRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Admin\Hall;
+
+use App\Models\Seat;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateHallRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'is_available' => ['required', 'boolean'],
+            'updated_seats' => ['required', 'array'],
+            'updated_seats.*' => ['nullable', 'array'],
+            'updated_seats.*.id' => ['required', 'string'],
+            'updated_seats.*.type' => ['required', 'string', Rule::in(Seat::SEAT_TYPES)],
+        ];
+    }
+}
