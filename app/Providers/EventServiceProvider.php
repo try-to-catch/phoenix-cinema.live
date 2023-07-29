74484c7d22447b16ffb6ed1604478775b674983d
Kostya <koooooostaaa@gmail.com>
2023-07-29 21:15:26 +0300

    register observers for the HallTemplate model and Movie model

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\HallTemplate;
+use App\Models\Movie;
+use App\Observers\HallTemplateObserver;
+use App\Observers\MovieObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+    ];
+
+    protected $observers = [
+        HallTemplate::class => [HallTemplateObserver::class],
+        Movie::class => [MovieObserver::class],
     ];
 
     /**
