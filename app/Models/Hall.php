e9aff3f1b9cb0a896b1c61314e7624f07100b0a7
Kostya <koooooostaaa@gmail.com>
2023-08-01 20:49:17 +0300

    set up a new relationship with screenings

--- app/Models/Hall.php
+++ app/Models/Hall.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Hall extends Model
@@ -19,5 +20,10 @@ class Hall extends Model
     public function seats(): MorphMany
     {
         return $this->morphMany(Seat::class, 'seatable');
+    }
+
+    public function screenings(): HasMany
+    {
+        return $this->hasMany(Screening::class);
     }
 }
