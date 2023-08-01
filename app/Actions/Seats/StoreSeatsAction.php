1f17eaecc21ff5d6a2d399e16594fc95c8b85ce9
Kostya <koooooostaaa@gmail.com>
2023-08-01 16:45:37 +0300

    update the action, add the option to use the provided(default) seat template

--- app/Actions/Seats/StoreSeatsAction.php
+++ app/Actions/Seats/StoreSeatsAction.php
@@ -2,15 +2,30 @@
 
 namespace App\Actions\Seats;
 
+use App\Models\Hall;
 use App\Models\HallTemplate;
 
 /**
  * This action is used to store seats in the hall in the correct format.
+ * If no seats are provided, it will use the default seats template.
  */
 class StoreSeatsAction
 {
-    public function handle(HallTemplate $hall, array $seats): void
+    protected array $seats = [
+        [
+            ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard']
+        ],
+        [
+            ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'],
+        ],
+        [
+            ['type' => 'premium'], ['type' => 'premium'], ['type' => 'premium'], ['type' => 'premium'],
+        ]
+    ];
+
+    public function handle(HallTemplate|Hall $hall, array $seats = []): void
     {
+        $seats = empty($seats) ? $this->seats : $seats;
         $preparedSeats = [];
 
         foreach ($seats as $rowIndex => $row) {
