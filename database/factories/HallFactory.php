0e10c2b192db8b81995b86a429ed85f992529c54
Kostya <koooooostaaa@gmail.com>
2023-08-01 16:46:10 +0300

    create HallFactory

---
+++ database/factories/HallFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Actions\Seats\StoreSeatsAction;
+use App\Models\Hall;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Hall>
+ */
+class HallFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(rand(1, 3), true),
+        ];
+    }
+
+    public function configure(): Factory
+    {
+        /** @var StoreSeatsAction $storeSeats */
+        $storeSeats = app(StoreSeatsAction::class);
+
+        return $this->afterCreating(fn($hall) => $storeSeats->handle($hall));
+    }
+}
