49e699b07c46abdc00da2d0f8ef2a3ec847636aa
Kostya <koooooostaaa@gmail.com>
2023-08-01 20:59:07 +0300

    create available scope

--- app/Models/HallTemplate.php
+++ app/Models/HallTemplate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -23,5 +24,10 @@ class HallTemplate extends Model
     public function seats(): MorphMany
     {
         return $this->morphMany(Seat::class, 'seatable');
+    }
+
+    public function scopeAvailable(Builder $query): Builder
+    {
+        return $query->where('is_available', true);
     }
 }
