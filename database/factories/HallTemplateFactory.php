5e645e4cb091816dc19f6e265ef847052bd32ac4
Kostya <koooooostaaa@gmail.com>
2023-08-01 16:47:50 +0300

    update HallTemplate factory, currently it extends HallFactory

--- database/factories/HallTemplateFactory.php
+++ database/factories/HallTemplateFactory.php
@@ -2,47 +2,23 @@
 
 namespace Database\Factories;
 
-use App\Actions\Seats\StoreSeatsAction;
 use App\Models\HallTemplate;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends Factory<HallTemplate>
  */
-class HallTemplateFactory extends Factory
+class HallTemplateFactory extends HallFactory
 {
     /**
-     * Define the model's default state.
+     * Extend the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
-        return [
-            'title' => fake()->words(rand(1, 3), true),
-            'is_available' => true,
-        ];
-    }
-
-    public function configure(): HallTemplateFactory
-    {
-        /** @var StoreSeatsAction $storeSeats */
-        $storeSeats = app(StoreSeatsAction::class);
-
-        return $this->afterCreating(function (HallTemplate $hall) use ($storeSeats) {
-            $seats = [
-                [
-                    ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard']
-                ],
-                [
-                    ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'], ['type' => 'standard'],
-                ],
-                [
-                    ['type' => 'premium'], ['type' => 'premium'], ['type' => 'premium'], ['type' => 'premium'],
-                ]
-            ];
-
-            $storeSeats->handle($hall, $seats);
+        return tap(parent::definition(), function (array &$attributes) {
+            $attributes['is_available'] = true;
         });
     }
 }
