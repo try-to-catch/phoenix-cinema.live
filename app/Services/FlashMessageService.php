f4c86d0dca0c6059e34fcf7fd3302aac130a16a9
Kostya <koooooostaaa@gmail.com>
2023-08-02 19:53:49 +0300

    implement failure method for failure message

--- app/Services/FlashMessageService.php
+++ app/Services/FlashMessageService.php
@@ -9,10 +9,11 @@ class FlashMessageService
      */
     public function success(string $message): void
     {
-        session()->flash('message', [
-            'type' => 'success',
-            'text' => $message,
-        ]);
+        session()->flash('message', ['type' => 'success', 'text' => $message]);
     }
 
+    public function failure(string $message): void
+    {
+        session()->flash('message', ['type' => 'failure', 'text' => $message]);
+    }
 }
