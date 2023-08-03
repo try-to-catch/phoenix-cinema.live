b16af27bbaeb5c053b547276c65ccc2031560d40
Kostya <koooooostaaa@gmail.com>
2023-08-03 21:45:07 +0300

    create new controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+        return Inertia::render('Home', [
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
