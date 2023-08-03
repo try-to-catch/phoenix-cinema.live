7ccae1427f03e0b305b9dd576bb5a4cce18431a3
Kostya <koooooostaaa@gmail.com>
2023-08-03 21:44:52 +0300

    create new components

---
+++ resources/js/Components/Icons/ApplicationLogo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps<{ styles?: string }>()
+</script>
+
+<template>
+    <div class="text-xl md:text-2xl text-secondary font-semibold"
+         :class="[$style['text-shadow'],styles]">
+        PHOENIX
+    </div>
+</template>
+
+<style module>
+.text-shadow:hover {
+    text-shadow: 1px 1px 2px #F37515, 0 0 1em #F37515, 0 0 0.2em #F37515;
+}
+</style>
