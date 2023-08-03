fd04d3479ab304ed64fcc9a51d599b2086dde846
Kostya <koooooostaaa@gmail.com>
2023-08-03 21:39:11 +0300

    rename Welcome page to Home, update Home page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import {Head} from '@inertiajs/vue3';
+import MainLayout from "@/Layouts/MainLayout.vue";
+import {ref} from "vue";
+import AnimatedCarousel from "@/Components/Icons/AnimatedCarousel.vue";
+
+const activeSlide = ref(0);
+
+const changeActiveSlide = (id: number) => {
+    activeSlide.value = id;
+}
+</script>
+
+<template>
+    <Head>
+        <title>Головна сторінка</title>
+    </Head>
+
+    <MainLayout>
+        <div class="py-4">
+            <section class="container">
+                <animated-carousel/>
+            </section>
+        </div>
+    </MainLayout>
+</template>
+
+<style scoped></style>
