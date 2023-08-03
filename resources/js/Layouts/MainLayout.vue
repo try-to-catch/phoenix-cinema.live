2b9f33f78b4e0a7b019938a81798de89d4f11438
Kostya <koooooostaaa@gmail.com>
2023-08-03 21:45:38 +0300

    create new layout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+import ApplicationLogo from "@/Components/Icons/ApplicationLogo.vue";
+import {Link} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div class="font-sans-serif w-full min-h-screen flex flex-col bg-primary">
+        <header>
+            <div class="container py-2 flex items-center justify-between">
+                <div>
+                    <Link :href="route('home')">
+                        <ApplicationLogo/>
+                    </Link>
+                </div>
+
+                <div class="text-secondary  space-x-2 flex">
+                    <Link :href="route('login')">Вхід</Link>
+                    <div>|</div>
+                    <Link :href="route('register')">Реєстрація</Link>
+                </div>
+            </div>
+
+        </header>
+
+        <main class="flex-grow">
+            <slot/>
+        </main>
+
+        <footer class="bg-tertiary w-full flex justify-center border-t border-secondary">
+            <div class="text-sm py-4 text-secondary">© 2022-2023 PX-Phoenix Holding. Всі права захищено</div>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
