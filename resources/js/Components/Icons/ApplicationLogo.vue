a47e0c4d988c45a801c721fb435aa85aff344be8
Kostya <koooooostaaa@gmail.com>
2023-08-04 21:40:18 +0300

    update ApplicationLogo with hover effect

--- resources/js/Components/Icons/ApplicationLogo.vue
+++ resources/js/Components/Icons/ApplicationLogo.vue
@@ -11,7 +11,14 @@ const props = defineProps<{ styles?: string }>()
 </template>
 
 <style module>
+
+.text-shadow {
+    transition: 0.3s ease-in-out;
+}
+
 .text-shadow:hover {
+    transition: 0.5s ease-in-out;
     text-shadow: 1px 1px 2px #F37515, 0 0 1em #F37515, 0 0 0.2em #F37515;
 }
+
 </style>
