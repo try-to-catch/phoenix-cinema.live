e88a892ad31035c4a75a11cafb0a71d5620d525e
Kostya <koooooostaaa@gmail.com>
2023-08-04 21:43:08 +0300

    update HomePage, add new sections

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,14 +1,10 @@
 <script lang="ts" setup>
 import {Head} from '@inertiajs/vue3';
 import MainLayout from "@/Layouts/MainLayout.vue";
-import {ref} from "vue";
-import AnimatedCarousel from "@/Components/AnimatedCarousel.vue";
-
-const activeSlide = ref(0);
-
-const changeActiveSlide = (id: number) => {
-    activeSlide.value = id;
-}
+import MovieBannerWithNotes from "@/Components/Banners/MovieBannerWithNotes.vue";
+import H3Title from "@/Components/Titles/H3Title.vue";
+import MovieList from "@/Components/Movies/MovieList.vue";
+import AdvantagesSection from "@/Components/Advantages/AdvantagesSection.vue";
 </script>
 
 <template>
@@ -17,11 +13,21 @@ const changeActiveSlide = (id: number) => {
     </Head>
 
     <MainLayout>
-        <div class="py-4">
-            <section class="container">
-                <animated-carousel/>
-            </section>
-        </div>
+        <section class="py-1">
+            <div class="container">
+                <MovieBannerWithNotes class="h-[400px]"/>
+            </div>
+        </section>
+
+        <AdvantagesSection class="mt-12"/>
+
+        <section class="mt-12">
+            <div class="container">
+                <H3Title>Зараз у кіно</H3Title>
+
+                <MovieList class="mt-5"/>
+            </div>
+        </section>
     </MainLayout>
 </template>
 
