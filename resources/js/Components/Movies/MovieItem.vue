e9df9b51fdb6f163f9941044002243d10edd223d
Kostya <koooooostaaa@gmail.com>
2023-08-05 21:15:19 +0300

    update the components

--- resources/js/Components/Movies/MovieItem.vue
+++ resources/js/Components/Movies/MovieItem.vue
@@ -1,20 +1,60 @@
 <script lang="ts" setup>
-import {defineProps} from "vue";
+import {defineProps, ref} from "vue";
 
 defineProps<{ title: string, image_src: string }>()
 
+const isHovered = ref(false)
 </script>
 
 <template>
     <li :title="title"
-        class="min-w-[218px] w-full flex justify-center min-h-[322px] cursor-pointer relative">
+        class="min-w-[218px] w-full flex justify-center min-h-[322px] cursor-pointer relative"
+        @mouseover="isHovered =true"
+        @mouseleave="isHovered = false"
+    >
         <div class="">
-            <img :alt="`${title} poster`" :src="image_src">
+            <img :alt="`${title} poster`" :src="image_src" class="rounded-md">
 
             <div class="w-8 h-8 text-xs absolute rounded-md top-2 left-2 bg-white flex justify-center items-center">
                 12+
             </div>
+
+            <transition name="liftUp">
+                <div v-if="isHovered"
+                     class="absolute bottom-0 w-full h-full rounded-md bg-gradient-to-t from-black to-transparent p-6 flex flex-col justify-end right-[50%] translate-x-1/2">
+                    <h6 class="text-sm  text-white font-medium">
+                        Джокер повернення додому
+                    </h6>
+
+                    <ul class="my-6 space-y-2">
+                        <li class="leading-5">
+                            <div class="text-xs text-gray-400 mb-1 font-medium">Жанр</div>
+                            <div class="text-xs text-white">Інстрія, Фантастика, Драмма</div>
+                        </li>
+                        <li class="leading-5">
+                            <div class="text-xs text-gray-400 mb-1 font-medium">Режисер</div>
+                            <div class="text-xs text-white">Крістафер Нолан</div>
+                        </li>
+                    </ul>
+
+                    <div class="text-xs  text-secondary font-medium">20 липня - 03 серпня</div>
+                </div>
+            </transition>
+
         </div>
 
     </li>
 </template>
+
+
+<style scoped>
+.liftUp-enter-active,
+.liftUp-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.liftUp-enter-from,
+.liftUp-leave-to {
+    opacity: 0;
+}
+</style>
--- resources/js/Components/Movies/MovieList.vue
+++ resources/js/Components/Movies/MovieList.vue
@@ -4,17 +4,14 @@ import MovieItem from "@/Components/Movies/MovieItem.vue";
 </script>
 
 <template>
-    <ul class="overflow-hidden grid gap-5 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+    <ul class="overflow-hidden grid gap-5 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
         <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
-        <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
-        <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
-        <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
-        <MovieItem title="Joker" image_src="/images/defaults/joker.jpg"/>
+
     </ul>
 </template>
 
