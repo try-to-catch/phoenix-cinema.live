dc597f7b2e3861ceb845c2cd9bc20c6e1f0df27c
Kostya <koooooostaaa@gmail.com>
2023-08-05 21:11:55 +0300

    create MovieBanner migration

---
+++ database/migrations/2023_08_05_144558_create_movie_banners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movie_banners', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('movie_id')->constrained()->cascadeOnDelete();
+            $table->string('image');
+            $table->string('description');
+            $table->string('fact');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (!app()->isProduction()) {
+            Schema::dropIfExists('movie_banners');
+        }
+    }
+};
