3cefa7563a1dd7c0af0b7c9070c11388b0a5b9a7
Kostya <koooooostaaa@gmail.com>
2023-08-05 21:11:23 +0300

    create MovieBanner model

---
+++ app/Models/MovieBanner.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MovieBanner extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'image',
+        'short_description',
+        'fascinating_fact',
+    ];
+
+    public function imagePath(): Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => '/storage/' . $attributes['image'],
+        );
+    }
+}
