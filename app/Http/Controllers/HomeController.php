9d0f498fd7777043157b8058cef5676d6636e799
Kostya <koooooostaaa@gmail.com>
2023-08-05 21:23:46 +0300

    update HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MovieBanner\MovieBannerResource;
+use App\Models\Movie;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
+        $movieWithBanner = Movie::hasBanner()->inRandomOrder()->take(1)->with(['banner', 'genres'])->first();
         return Inertia::render('Home', [
-            'phpVersion' => PHP_VERSION,
+            'banner' => MovieBannerResource::make($movieWithBanner),
         ]);
     }
 }
