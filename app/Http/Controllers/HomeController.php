bbc48271405fa98423806d5d0300ec7642f0df97
Kostya <koooooostaaa@gmail.com>
2023-08-06 20:04:51 +0300

    extend return data in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Movie\MovieListResource;
 use App\Http\Resources\MovieBanner\MovieBannerResource;
 use App\Models\Movie;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeController extends Controller
 {
-    public function __invoke()
+
+    protected const ROW_LENGTH = 12;
+
+    public function __invoke(): Response
     {
-        $movieWithBanner = Movie::hasBanner()->inRandomOrder()->take(1)->with(['banner', 'genres'])->first();
+        $movieWithBanner = Movie::hasBanner()->inRandomOrder()->with(['banner', 'genres'])->first();
+        $selectedColumns = ['id', 'title', 'slug', 'thumbnail', 'age_restriction', 'director', 'start_showing', 'end_showing'];
+
+        $movies = Movie::currentlyScreeningWithGenres()->orderBy('priority')->take(self::ROW_LENGTH)->get($selectedColumns);
+        $futureMovies = Movie::screeningSoonWithGenres()->orderBy('start_showing')->take(self::ROW_LENGTH)->get($selectedColumns);
+
         return Inertia::render('Home', [
-            'banner' => MovieBannerResource::make($movieWithBanner),
+            'banner' => MovieBannerResource::make($movieWithBanner)->resolve(),
+            'movies' => MovieListResource::collection($movies)->resolve(),
+            'future_movies' => MovieListResource::collection($futureMovies)->resolve(),
         ]);
     }
 }
