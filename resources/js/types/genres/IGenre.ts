45daf1681d286fc99b75b3a2ebdd8736f8bdf4a9
Kostya <koooooostaaa@gmail.com>
2023-08-06 20:01:50 +0300

    create new types

---
+++ resources/js/types/genres/IGenre.ts
@@ -0,0 +1,4 @@
+export interface IGenre {
+    readonly id: string;
+    readonly name: string;
+}
---
+++ resources/js/types/movies/IMovieCard.ts
@@ -0,0 +1,13 @@
+import type {IGenre} from "@/types/genres/IGenre";
+
+export interface IMovieCard {
+    readonly id: string;
+    title: string;
+    readonly slug: string;
+    thumbnail: string;
+    age_restriction: string;
+    director: string;
+    start_showing: string;
+    end_showing: string;
+    genres: IGenre[];
+}
