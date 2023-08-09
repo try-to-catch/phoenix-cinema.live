301696f6731d725e448ca9b4aeb8e45d06455aa1
Kostya <koooooostaaa@gmail.com>
2023-08-09 15:51:39 +0300

    rename component (MovieItem -> MovieCard)

--- resources/js/Components/Movies/MovieList.vue
+++ resources/js/Components/Movies/MovieList.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-import MovieItem from "@/Components/Movies/MovieItem.vue";
+import MovieCard from "@/Components/Movies/MovieCard.vue";
 import type {IMovieCard} from "@/types/movies/IMovieCard";
 
 const {movies} = defineProps<{ movies: readonly IMovieCard[] }>()
 </script>
 
 <template>
-  <ul class="overflow-hidden grid gap-5 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-    <MovieItem v-for="movie in movies" :movie="movie" :key="movie.id"/>
-  </ul>
+    <ul class="overflow-hidden grid gap-5 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+        <MovieCard v-for="movie in movies" :movie="movie" :key="movie.id"/>
+    </ul>
 </template>
