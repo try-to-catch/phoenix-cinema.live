87f14fc20c72571562c2baab90b28db6b2fa32be
Kostya <koooooostaaa@gmail.com>
2023-08-10 21:18:18 +0300

    update prop types

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -9,43 +9,43 @@ import type {IMovieCard} from "@/types/movies/IMovieCard";
 import type {IBanner} from "@/types/banners/IBanner";
 
 const {banner, movies, future_movies} = defineProps<{
-    banner: IBanner,
-    movies: readonly IMovieCard[],
-    future_movies: readonly IMovieCard[]
+  banner: IBanner;
+  movies: readonly Readonly<IMovieCard>[];
+  future_movies: readonly Readonly<IMovieCard>[]
 }>()
 
 </script>
 
 <template>
-    <Head>
-        <title>Головна сторінка</title>
-    </Head>
+  <Head>
+    <title>Головна сторінка</title>
+  </Head>
 
-    <MainLayout>
-        <section class="pt-1">
-            <div class="container">
-                <MovieBannerWithNotes :banner="banner" class="h-[400px]"/>
-            </div>
-        </section>
+  <MainLayout>
+    <section class="pt-1">
+      <div class="container">
+        <MovieBannerWithNotes :banner="banner" class="h-[400px]"/>
+      </div>
+    </section>
 
-        <AdvantagesSection class="my-12"/>
+    <AdvantagesSection class="my-12"/>
 
-        <section class="mb-12">
-            <div class="container">
-                <H3Title>Зараз у кіно</H3Title>
+    <section class="mb-12">
+      <div class="container">
+        <H3Title>Зараз у кіно</H3Title>
 
-                <MovieList class="mt-5" :movies="movies"/>
-            </div>
-        </section>
+        <MovieList class="mt-5" :movies="movies"/>
+      </div>
+    </section>
 
-        <section class="mb-12">
-            <div class="container">
-                <H3Title>Скоро у кіно</H3Title>
+    <section class="mb-12">
+      <div class="container">
+        <H3Title>Скоро у кіно</H3Title>
 
-                <MovieList class="mt-5" :movies="future_movies"/>
-            </div>
-        </section>
-    </MainLayout>
+        <MovieList class="mt-5" :movies="future_movies"/>
+      </div>
+    </section>
+  </MainLayout>
 </template>
 
 <style scoped></style>
