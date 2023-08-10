08f4d4c29bc1be6707a96cdf044be3d4734c1a1b
Kostya <koooooostaaa@gmail.com>
2023-08-10 21:20:39 +0300

    wrap the data into resources in MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Screening\OrganizeScreeningsAction;
+use App\Http\Resources\Movie\MovieResource;
+use App\Http\Resources\Screening\OrganizedScreeningListResource;
 use App\Models\Movie;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -11,12 +14,15 @@ class MovieController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Movie $movie): Response
+    public function show(Movie $movie, OrganizeScreeningsAction $organizeScreeningsAction): Response
     {
-        $movie->load('genres');
+        $movie->load(['genres', 'screenings']);
+
+        $screenings = $organizeScreeningsAction->handle($movie->screenings);
+
         return Inertia::render('Movies/Show', [
-            'movie' => $movie->toArray(),
-            'screenings' => $movie->screenings()->get()->toArray(),
+            'movie' => MovieResource::make($movie)->resolve(),
+            'screenings' => OrganizedScreeningListResource::collection($screenings)->resolve(),
         ]);
     }
 }
