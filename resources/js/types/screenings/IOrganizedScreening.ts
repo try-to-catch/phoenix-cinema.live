c7234106036a07e88c59b053d9d4c2f873fb9b57
Kostya <koooooostaaa@gmail.com>
2023-08-11 20:07:29 +0300

    create IOrganizedScreening type

---
+++ resources/js/types/screenings/IOrganizedScreening.ts
@@ -0,0 +1,6 @@
+import type {IOrganizedScreeningItem} from "@/types/screenings/IOrganizedScreeningItem";
+
+export interface IOrganizedScreening {
+    date: string;
+    screenings: IOrganizedScreeningItem[];
+}
