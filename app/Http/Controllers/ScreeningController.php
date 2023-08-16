b06a8782558281bcf67fe0fc01aafeaee049a006
Kostya <koooooostaaa@gmail.com>
2023-08-16 20:34:26 +0300

    wrap screening into ScreeningInfo resource

--- app/Http/Controllers/ScreeningController.php
+++ app/Http/Controllers/ScreeningController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\Admin\Seat\SeatResource;
+use App\Http\Resources\Screening\ScreeningInfoResource;
 use App\Models\Screening;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -14,11 +15,11 @@ class ScreeningController extends Controller
      */
     public function show(Screening $screening): Response
     {
-        $screening->load('seats');
+        $screening->load(['seats', 'movie', 'hall']);
         return Inertia::render('Screenings/Show',
             [
                 'seating_plan' => SeatResource::make($screening->seats)->resolve(),
-                'screening' => $screening,
+                'screening' => ScreeningInfoResource::make($screening)->resolve()
             ]);
     }
 }
