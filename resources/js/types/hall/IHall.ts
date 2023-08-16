a0c0dd7eda49b0ee380b054e8d8e9d47ffe187ca
Kostya <koooooostaaa@gmail.com>
2023-08-16 20:31:38 +0300

    create new types

---
+++ resources/js/types/hall/IHall.ts
@@ -0,0 +1,5 @@
+export interface IHall {
+    readonly id: string;
+    readonly address: string;
+    readonly number: number;
+}
---
+++ resources/js/types/movies/IMovieDetails.ts
@@ -0,0 +1,6 @@
+export interface IMovieDetails {
+    id: string;
+    title: string;
+    age_restriction: string;
+    thumbnail: string;
+}
