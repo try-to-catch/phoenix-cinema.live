ed55f052f2ef55325cfa85a3855fbea78901b774
Kostya <koooooostaaa@gmail.com>
2023-08-18 20:28:34 +0300

    add index method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Actions\Screening\OrganizeScreeningsAction;
+use App\Actions\Shared\FormatPaginationMetaLinksAction;
+use App\Http\Resources\Movie\MovieListResource;
 use App\Http\Resources\Movie\MovieResource;
 use App\Http\Resources\Screening\OrganizedScreeningListResource;
 use App\Models\Movie;
@@ -11,6 +13,23 @@ use Inertia\Response;
 
 class MovieController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(FormatPaginationMetaLinksAction $formatLinksAction): Response
+    {
+        $selectedColumns = ['id', 'title', 'slug', 'thumbnail', 'age_restriction', 'director', 'start_showing', 'end_showing'];
+
+        //TODO add caching of the movies and future movies
+        $movies = Movie::currentlyScreeningWithGenres()->orderByDesc('priority')->get($selectedColumns);
+        $futureMovies = Movie::screeningSoonWithGenres()->orderBy('start_showing')->select($selectedColumns)->paginate(18);
+
+        return Inertia::render('Movies/Index', [
+            'movies' => MovieListResource::collection($movies)->resolve(),
+            'future_movies' => MovieListResource::collection($futureMovies),
+        ]);
+    }
+
     /**
      * Display the specified resource.
      */
