57d6acad64a7c03d51ba7b6c6f72fa9e854b250f
Kostya <koooooostaaa@gmail.com>
2023-08-19 20:29:52 +0300

    implement the following condition: if (user auth), then instead of links to log in and registration show a link to profile and log out, otherwise - vice versa

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
 import ApplicationLogo from "@/Components/Icons/ApplicationLogo.vue";
 import {Link} from "@inertiajs/vue3";
+import DropdownLink from "@/Components/Breeze/DropdownLink.vue";
+import Dropdown from "@/Components/Breeze/Dropdown.vue";
+import AngleDown from "@/Components/Icons/AngleDown.vue";
 </script>
 
 <template>
@@ -14,10 +17,33 @@ import {Link} from "@inertiajs/vue3";
           </Link>
         </div>
 
-        <div class="text-secondary space-x-2 flex text-sm md:text-base">
+        <div v-if="!$page.props.auth.user" class="text-secondary space-x-2 flex text-sm md:text-base">
           <Link :href="route('login')">Вхід</Link>
           <div class="sm:block hidden">|</div>
           <Link :href="route('register')">Реєстрація</Link>
+        </div>
+
+        <div v-if="$page.props.auth.user" class="ml-3 relative">
+          <Dropdown align="right" width="48">
+            <template #trigger>
+              <span class="inline-flex rounded-md">
+                <button
+                    class="inline-flex items-center px-3 py-2 border border-transparent sm:text-base text-sm leading-4 font-medium rounded-md text-secondary bg-tertiary sm:bg-primary hover:text-orange-600 focus:outline-none transition ease-in-out duration-300"
+                    type="button"
+                >
+                  {{ $page.props.auth.user.name }}
+                  <AngleDown/>
+                </button>
+              </span>
+            </template>
+
+            <template #content>
+              <DropdownLink :href="route('profile.edit')"> Profile</DropdownLink>
+              <DropdownLink :href="route('logout')" as="button" method="post">
+                Log Out
+              </DropdownLink>
+            </template>
+          </Dropdown>
         </div>
       </div>
 
