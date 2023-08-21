06924da2d0277e8d2d8d255ac6d05721d3f2decb
Kostya <koooooostaaa@gmail.com>
2023-08-21 19:44:23 +0300

    cosmetic changes

--- resources/js/Components/Breeze/Modal.vue
+++ resources/js/Components/Breeze/Modal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, watch } from 'vue';
+import {computed, onMounted, onUnmounted, watch} from 'vue';
 
 const props = withDefaults(
     defineProps<{
@@ -60,7 +60,7 @@ const maxWidthClass = computed(() => {
 <template>
     <Teleport to="body">
         <Transition leave-active-class="duration-200">
-            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50" scroll-region>
+            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50">
                 <Transition
                     enter-active-class="ease-out duration-300"
                     enter-from-class="opacity-0"
@@ -70,7 +70,7 @@ const maxWidthClass = computed(() => {
                     leave-to-class="opacity-0"
                 >
                     <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
-                        <div class="absolute inset-0 bg-gray-500 opacity-75" />
+                        <div class="absolute inset-0 bg-primary opacity-70"/>
                     </div>
                 </Transition>
 
@@ -84,10 +84,10 @@ const maxWidthClass = computed(() => {
                 >
                     <div
                         v-show="show"
-                        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
+                        class="mb-6 bg-tertiary rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
                         :class="maxWidthClass"
                     >
-                        <slot v-if="show" />
+                        <slot v-if="show"/>
                     </div>
                 </Transition>
             </div>
--- resources/js/Components/Breeze/PrimaryButton.vue
+++ resources/js/Components/Breeze/PrimaryButton.vue
@@ -1,9 +1,9 @@
 <template>
-  <button
-      class="w-full text-white bg-secondary hover:bg-orange-600 duration-300 ease-in-out focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-  >
-    <slot/>
-  </button>
+    <button
+        class="w-full text-white bg-secondary hover:bg-orange-600 duration-300 ease-in-out focus:ring-2 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-5 py-2.5"
+    >
+        <slot/>
+    </button>
 </template>
 <script setup lang="ts">
 </script>
