c1448cd7432531ea2c21e9aa5ada1475f822626d
Kostya <koooooostaaa@gmail.com>
2023-08-28 21:58:25 +0300

    create new components

---
+++ resources/js/Components/Titles/H5Title.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ text?: string }>()
+</script>
+
+<template>
+  <h5 class="font-medium text-white text-base sm:text-lg">
+    {{ text }}
+    <template v-if="$slots.default && !text">
+      <slot/>
+    </template>
+  </h5>
+</template>
