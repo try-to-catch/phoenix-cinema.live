ef3c7fbe9c357bb131a520aab7236fd2737effdd
Kostya <koooooostaaa@gmail.com>
2023-08-28 22:04:28 +0300

    update the components make significantly more flexible

--- resources/js/Components/Breeze/PrimaryButton.vue
+++ resources/js/Components/Breeze/PrimaryButton.vue
@@ -1,9 +1,9 @@
 <template>
-    <button
-        class="w-full text-white bg-secondary hover:bg-orange-600 duration-300 ease-in-out focus:ring-2 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-5 py-2.5"
-    >
-        <slot/>
-    </button>
+  <button
+      class="w-full text-white bg-secondary hover:bg-orange-550 duration-300 ease-in-out focus:ring-2 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-5 py-2.5"
+  >
+    <slot/>
+  </button>
 </template>
 <script setup lang="ts">
 </script>
--- resources/js/Components/Titles/H3Title.vue
+++ resources/js/Components/Titles/H3Title.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ text?: string }>()
+</script>
 
 <template>
   <h3 class="md:text-3xl sm:text-2xl text-2xl font-bold text-white">
-    <slot/>
+    {{ text }}
+    <template v-if="$slots.default && !text">
+      <slot/>
+    </template>
   </h3>
 </template>
