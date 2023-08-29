5f25f596f917d09aad71ec03cf6fc247b10939a6
Kostya <koooooostaaa@gmail.com>
2023-08-29 16:28:58 +0300

    code styling changes

--- resources/js/Components/Modals/AuthSuggestionModal.vue
+++ resources/js/Components/Modals/AuthSuggestionModal.vue
@@ -7,53 +7,53 @@ import {ref} from "vue";
 
 const isOpen = ref(false)
 const modalController = ref<{
-  resolve: (value: boolean) => void,
-  reject: (reason?: any) => void
+    resolve: (value: boolean) => void,
+    reject: (reason?: any) => void
 } | null>(null)
 
-function redirectToLogin() {
-  router.visit(route('login'))
+const redirectToLogin = () => {
+    router.visit(route('login'))
 }
 
-function rejectAuth() {
-  isOpen.value = false
-  modalController.value?.resolve(false)
+const rejectAuth = () => {
+    isOpen.value = false
+    modalController.value?.resolve(false)
 }
 
-async function open() {
-  const modalPromise = new Promise((resolve: (value: boolean) => void, reject) => {
-    modalController.value = {resolve, reject}
-  });
+const open = () => {
+    const modalPromise = new Promise((resolve: (value: boolean) => void, reject) => {
+        modalController.value = {resolve, reject}
+    });
 
-  isOpen.value = true
-  return modalPromise;
+    isOpen.value = true
+    return modalPromise;
 }
 
 defineExpose<{ open: () => Promise<boolean> | never }>({open})
 </script>
 
 <template>
-  <Modal max-width="2xl" :show="isOpen">
-    <div class="p-6">
-      <div class="flex flex-col items-center">
-        <div class="w-32">
-          <img src="/images/emoji/thinking-face.png" alt="thinking face emoji" class="w-full h-full">
+    <Modal max-width="2xl" :show="isOpen">
+        <div class="p-6">
+            <div class="flex flex-col items-center">
+                <div class="w-32">
+                    <img src="/images/emoji/thinking-face.png" alt="thinking face emoji" class="w-full h-full">
+                </div>
+
+                <div class="font-medium text-white text-base sm:text-lg my-3">Бажаєте авторизуватися?</div>
+                <div class="text-sm sm:text-sm text-center text-neutral-400">
+                    Авторизація дасть змогу зберігати квитки в особистому кабінеті, і отримувати доступ до них з
+                    будь-якого пристрою
+                </div>
+            </div>
+
+            <div class="grid grid-cols-2 gap-3 mt-5">
+                <SecondaryButton @click="rejectAuth">Іншого разу</SecondaryButton>
+
+                <PrimaryButton role="link" @click="redirectToLogin">Так, звісно</PrimaryButton>
+            </div>
         </div>
-
-        <div class="font-medium text-white text-base sm:text-lg my-3">Бажаєте авторизуватися?</div>
-        <div class="text-sm sm:text-sm text-center text-neutral-400">
-          Авторизація дасть змогу зберігати квитки в особистому кабінеті, і отримувати доступ до них з
-          будь-якого пристрою
-        </div>
-      </div>
-
-      <div class="grid grid-cols-2 gap-3 mt-5">
-        <SecondaryButton @click="rejectAuth">Іншого разу</SecondaryButton>
-
-        <PrimaryButton role="link" @click="redirectToLogin">Так, звісно</PrimaryButton>
-      </div>
-    </div>
-  </Modal>
+    </Modal>
 </template>
 
 <style scoped>
