f0cec7e284379fe2a6fe6a33cf8177ab3cd1bc60
Kostya <koooooostaaa@gmail.com>
2023-09-03 19:13:22 +0300

    create Order migration

---
+++ database/migrations/2023_09_03_160348_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('screening_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignUuid('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('session_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (!app()->isProduction()) {
+            Schema::dropIfExists('orders');
+        }
+    }
+};
