ec867015470f4fdb5ce813e3f7dc6019fa2e91cc
Kostya <koooooostaaa@gmail.com>
2023-09-03 19:13:45 +0300

    create Order factory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'screening_id' => ScreeningFactory::new(),
+            'user_id' => UserFactory::new(),
+        ];
+    }
+}
