1153334245104659dfea7946f756dce5a23408f0
Kostya <koooooostaaa@gmail.com>
2023-09-03 15:20:44 +0300

    implement hasBgBlur attribute

--- resources/js/Components/Breeze/Modal.vue
+++ resources/js/Components/Breeze/Modal.vue
@@ -3,15 +3,17 @@ import {computed, onMounted, onUnmounted, watch} from 'vue';
 
 const props = withDefaults(
     defineProps<{
-      show?: boolean;
-      maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-      closeable?: boolean;
-      styles?: string;
+        show?: boolean;
+        maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+        closeable?: boolean;
+        styles?: string;
+        hasBgBlur?: boolean;
     }>(),
     {
-      show: false,
-      maxWidth: '2xl',
-      closeable: true,
+        show: false,
+        maxWidth: '2xl',
+        closeable: true,
+        hasBgBlur: false,
     }
 );
 
@@ -20,78 +22,80 @@ const emit = defineEmits(['close']);
 watch(
     () => props.show,
     () => {
-      if (props.show) {
-        document.body.style.overflow = 'hidden';
-      } else {
-        document.body.style.overflow = 'visible';
-      }
+        if (props.show) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'visible';
+        }
     }
 );
 
 const close = () => {
-  if (props.closeable) {
-    emit('close');
-  }
+    if (props.closeable) {
+        emit('close');
+    }
 };
 
 const closeOnEscape = (e: KeyboardEvent) => {
-  if (e.key === 'Escape' && props.show) {
-    close();
-  }
+    if (e.key === 'Escape' && props.show) {
+        close();
+    }
 };
 
 onMounted(() => document.addEventListener('keydown', closeOnEscape));
 
 onUnmounted(() => {
-  document.removeEventListener('keydown', closeOnEscape);
-  document.body.style.overflow = 'visible';
+    document.removeEventListener('keydown', closeOnEscape);
+    document.body.style.overflow = 'visible';
 });
 
 const maxWidthClass = computed(() => {
-  return {
-    sm: 'sm:max-w-sm',
-    md: 'sm:max-w-md',
-    lg: 'sm:max-w-lg',
-    xl: 'sm:max-w-xl',
-    '2xl': 'sm:max-w-2xl',
-  }[props.maxWidth];
+    return {
+        sm: 'sm:max-w-sm',
+        md: 'sm:max-w-md',
+        lg: 'sm:max-w-lg',
+        xl: 'sm:max-w-xl',
+        '2xl': 'sm:max-w-2xl',
+    }[props.maxWidth];
 });
 </script>
 
 <template>
-  <Teleport to="body">
-    <Transition leave-active-class="duration-200">
-      <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50">
-        <Transition
-            enter-active-class="ease-out duration-300"
-            enter-from-class="opacity-0"
-            enter-to-class="opacity-100"
-            leave-active-class="ease-in duration-200"
-            leave-from-class="opacity-100"
-            leave-to-class="opacity-0"
-        >
-          <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
-            <div class="absolute inset-0 bg-primary opacity-70"/>
-          </div>
-        </Transition>
+    <Teleport to="body">
+        <Transition leave-active-class="duration-200">
+            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50">
+                <Transition
+                    enter-active-class="ease-out duration-300"
+                    enter-from-class="opacity-0"
+                    enter-to-class="opacity-100"
+                    leave-active-class="ease-in duration-200"
+                    leave-from-class="opacity-100"
+                    leave-to-class="opacity-0"
+                >
+                    <div v-show="show" :class="{'backdrop-blur-sm' : hasBgBlur}"
+                         class="fixed inset-0 transform transition-all"
+                         @click="close">
+                        <div class="absolute inset-0 bg-primary opacity-70"/>
+                    </div>
+                </Transition>
 
-        <Transition
-            enter-active-class="ease-out duration-300"
-            enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to-class="opacity-100 translate-y-0 sm:scale-100"
-            leave-active-class="ease-in duration-200"
-            leave-from-class="opacity-100 translate-y-0 sm:scale-100"
-            leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        >
-          <div
-              v-show="show"
-              :class="[maxWidthClass,  styles]"
-              class="mb-6 bg-tertiary rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
-          >
-            <slot v-if="show"/>
-          </div>
+                <Transition
+                    enter-active-class="ease-out duration-300"
+                    enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+                    leave-active-class="ease-in duration-200"
+                    leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+                    leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                    <div
+                        v-show="show"
+                        :class="[maxWidthClass,  styles]"
+                        class="mb-6 bg-tertiary rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
+                    >
+                        <slot v-if="show"/>
+                    </div>
+                </Transition>
+            </div>
         </Transition>
-      </div>
-    </Transition>
-  </Teleport>
+    </Teleport>
 </template>
