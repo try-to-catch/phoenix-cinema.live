a4329fdb104db175b2ae481c79afa5f9d136ad4b
Kostya <koooooostaaa@gmail.com>
2023-09-11 17:54:39 +0300

    fix eslint issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,13 @@
 /* eslint-env node */
 module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:vue/vue3-recommended'],
-    parser: '@typescript-eslint/parser',
+    parser: 'vue-eslint-parser',
     plugins: ['@typescript-eslint'],
     parserOptions: {
+        parser: '@typescript-eslint/parser',
         project: true,
         tsconfigRootDir: __dirname,
+        extraFileExtensions: ['.vue'],
     },
     root: true,
 };
