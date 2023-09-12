a17bcf9c53c638b80f9dafe27e72f2e41ffcf643
Kostya <koooooostaaa@gmail.com>
2023-09-12 19:47:59 +0300

    create CreateNewOrder action

---
+++ app/Actions/Order/CreateNewOrderAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions\Order;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class CreateNewOrderAction
+{
+    /**
+     * Create new order.
+     */
+    public function handle(Request $request, $screeningId) : Builder|Model
+    {
+        $newOrder = ['screening_id' => $screeningId];
+
+        if ($request->user()) {
+            $newOrder['user_id'] = $request->user()->id;
+        } else {
+            $newOrder['session_id'] = $request->session()->getId();
+        }
+
+        return Order::query()->create($newOrder);
+    }
+}
