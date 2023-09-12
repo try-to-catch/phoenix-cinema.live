6036fd756b15891854615a6634e060a39e60780a
Kostya <koooooostaaa@gmail.com>
2023-09-12 19:49:04 +0300

    update store method in OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Order\CreateNewOrderAction;
 use App\Actions\Payment\ProcessPaymentAction;
+use App\Actions\Seat\UpdateSeatsOrderIdAction;
 use App\Http\Requests\Order\StoreOrderRequest;
+use App\Models\Order;
+use App\Models\Seat;
+use App\Services\FlashMessageService;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -12,10 +18,24 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreOrderRequest $request, ProcessPaymentAction $paymentAction): Response
+    public function store(
+        StoreOrderRequest        $request,
+        ProcessPaymentAction     $paymentAction,
+        FlashMessageService      $flashMessageService,
+        UpdateSeatsOrderIdAction $updateSeatsOrderIdAction,
+        CreateNewOrderAction     $createNewOrderAction
+    ): RedirectResponse
     {
-//        $paymentAction->handle([]);
-        dd($request->all());
-        return Inertia::render('Screenings/Show');
+        $data = $request->validated();
+
+        if (!$paymentAction->handle($data['card_data'])) {
+            $flashMessageService->failure('Оплата не пройшла! Щось пішло не так');
+            return redirect()->back()->with('errors', ['card_data' => 'Реквізити картки невірні']);
+        }
+
+        $order = $createNewOrderAction->handle($request, Seat::find($data['seat_ids'][0])->hall->screening_id);
+        $updateSeatsOrderIdAction->handle($data['seat_ids'], $order->id);
+
+        return to_route('orders.show', $order);
     }
 }
