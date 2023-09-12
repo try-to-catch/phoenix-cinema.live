dc594aa9595ce26014ae789589e5c9243d460aa3
Kostya <koooooostaaa@gmail.com>
2023-09-12 19:39:04 +0300

    create Orders/Show page

---
+++ resources/js/Pages/Orders/Show.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Thanks 4 order</div>
+  <button>Download tickets</button>
+  <div><small>soon or later this page gonna be pretty</small></div>
+</template>
+
+<style scoped></style>
