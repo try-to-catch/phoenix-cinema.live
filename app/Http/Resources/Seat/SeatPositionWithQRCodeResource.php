9adc1025ae6aa435528e52e7eaab5eb601e6703d
Kostya <koooooostaaa@gmail.com>
2023-09-19 20:10:45 +0300

    create new resource

---
+++ app/Http/Resources/Seat/SeatPositionWithQRCodeResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Seat;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+class SeatPositionWithQRCodeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $qrCode = QrCode::format('png')->color(29, 32, 36)->style('round')->size(120)->generate(request()->url());
+
+        return [
+            'id' => $this['id'],
+            'seat_number' => $this['seat_number'],
+            'row_number' => $this['row_number'],
+            'qr_code' => base64_encode($qrCode),
+        ];
+    }
+}
