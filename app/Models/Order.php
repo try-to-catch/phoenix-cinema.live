cc6c8e8855be9ec297cf1972b65d22d26a2e3899
Kostya <koooooostaaa@gmail.com>
2023-09-20 18:34:24 +0300

    establish new relationships in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -15,4 +17,14 @@ class Order extends Model
         'screening_id',
         'session_id',
     ];
+
+    public function seats(): HasMany
+    {
+        return $this->hasMany(Seat::class);
+    }
+
+    public function screening(): BelongsTo
+    {
+        return $this->belongsTo(Screening::class);
+    }
 }
