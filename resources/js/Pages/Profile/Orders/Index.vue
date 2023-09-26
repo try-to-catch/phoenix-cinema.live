f72ad19a0c071b15b3a78c537f44b204ffa68226
Kostya <koooooostaaa@gmail.com>
2023-09-26 20:57:48 +0300

    create new pages

---
+++ resources/js/Pages/Profile/Orders/Index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import ProfileLayout from '@/Layouts/ProfileLayout.vue'
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Head title="Профіль - Замовлення" />
+  <ProfileLayout>
+    <div class=""></div>
+  </ProfileLayout>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Profile/Orders/Show.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="">Profile.Orders.Show</div>
+</template>
+
+<style scoped></style>
