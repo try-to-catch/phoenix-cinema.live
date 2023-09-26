106bf5bf247ccde36956151ef363e0d4183c6ee4
Kostya <koooooostaaa@gmail.com>
2023-09-26 21:10:34 +0300

    fix no banner issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,15 +14,17 @@ class HomeController extends Controller
 
     public function __invoke(): Response
     {
-        $movieWithBanner = Movie::hasBanner()->inRandomOrder()->with(['banner', 'genres'])->first();
+        $movieWithBanner = Movie::query()->hasBanner()->inRandomOrder()->with(['banner', 'genres'])->first();
         $selectedColumns = ['id', 'title', 'slug', 'thumbnail', 'age_restriction', 'director', 'start_showing', 'end_showing'];
 
         //TODO add caching of the movies and future movies
         $movies = Movie::currentlyScreeningWithGenres()->orderByDesc('priority')->take(self::ROW_LENGTH)->get($selectedColumns);
         $futureMovies = Movie::screeningSoonWithGenres()->orderBy('start_showing')->take(self::ROW_LENGTH / 2)->get($selectedColumns);
 
+        $banner = $movieWithBanner ? MovieBannerResource::make($movieWithBanner)->resolve() : null;
+
         return Inertia::render('Home', [
-            'banner' => MovieBannerResource::make($movieWithBanner)->resolve(),
+            'banner' => $banner,
             'movies' => MovieListResource::collection($movies)->resolve(),
             'futureMovies' => MovieListResource::collection($futureMovies)->resolve(),
         ]);
