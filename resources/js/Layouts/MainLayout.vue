95793c725b472e1aa8984ab692d290f017573a20
Kostya <koooooostaaa@gmail.com>
2023-09-29 20:47:59 +0300

    create .gitignore for debugbar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,12 +2,47 @@
 import ApplicationLogo from '@/Components/Icons/ApplicationLogo.vue'
 import { Link } from '@inertiajs/vue3'
 import DefaultDropdown from '@/Layouts/Shared/DefaultDropdown.vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+
+const pagesWithLongHeader = ['home', 'movies.index']
+
+const isHeaderLong = computed(() => {
+  console.log(pagesWithLongHeader.includes(route().current()!))
+  return pagesWithLongHeader.includes(route().current()!)
+})
+
+const scrollTop = ref(0)
+
+const updateScrollY = () => {
+  scrollTop.value = window.scrollY
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', updateScrollY)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', updateScrollY)
+})
+
+const isHeaderFixed = computed(() => {
+  return scrollTop.value > 50
+})
 </script>
 
 <template>
   <div class="font-sans-serif w-full min-h-screen flex flex-col bg-primary relative">
-    <header class="fixed w-full z-20 sm:bg-primary bg-tertiary h-[50px] border-b border-neutral-700 sm:border-none">
-      <div class="container py-2 flex items-center justify-between h-full">
+    <header
+      :class="{
+        'bg-tertiary shadow': isHeaderFixed,
+        'sm:bg-primary bg-tertiary': !isHeaderFixed,
+      }"
+      class="fixed w-full z-20 h-[50px] border-b border-neutral-700 sm:border-none duration-300 ease-in-out"
+    >
+      <div
+        :class="{ container: isHeaderLong, 'max-w-7xl mx-auto px-4 sm:px-6 lg:px-8': !isHeaderLong }"
+        class="py-2 flex items-center justify-between h-full"
+      >
         <div>
           <Link :href="route('home')">
             <ApplicationLogo />
--- resources/js/Layouts/ProfileLayout.vue
+++ resources/js/Layouts/ProfileLayout.vue
@@ -36,7 +36,7 @@ const activeRoute = computed(() => {
 <template>
   <div class="font-sans-serif">
     <div class="min-h-screen bg-primary relative">
-      <nav class="bg-tertiary shadow-md border-b border-neutral-700 sm:border-none fixed top-0 right-0 left-0 z-20">
+      <nav class="bg-tertiary shadow border-b border-neutral-700 sm:border-none fixed top-0 right-0 left-0 z-20">
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between h-[50px]">
