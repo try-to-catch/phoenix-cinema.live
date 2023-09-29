0b6ae3d87c1442ff72090414041b55a6c39203d5
Kostya <koooooostaaa@gmail.com>
2023-09-29 20:55:28 +0300

    update index method in OrderController

--- app/Http/Controllers/Profile/OrderController.php
+++ app/Http/Controllers/Profile/OrderController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Order\ProfileOrderListResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        return Inertia::render('Profile/Orders/Index');
+        $orders = Order::query()
+            ->select('id', 'screening_id', 'completed_at', 'created_at')
+            ->with([
+                'screening' => function ($query) {
+                    $query
+                        ->with(['movie' => fn ($query) => $query->select('id', 'title', 'slug')])
+                        ->select('id', 'movie_id', 'start_time');
+                },
+            ])
+            ->withCount('seats')
+            ->latest('created_at')
+            ->paginate();
+
+        return Inertia::render('Profile/Orders/Index', [
+            'orders' => ProfileOrderListResource::collection($orders),
+        ]);
     }
 
     /**
