f5e320d335c5356dac7694ae2ba27f242f117840
Kostya <koooooostaaa@gmail.com>
2023-09-29 20:54:51 +0300

    add completed_at column to fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -16,6 +17,7 @@ class Order extends Model
         'user_id',
         'screening_id',
         'session_id',
+        'completed_at',
     ];
 
     public function seats(): HasMany
@@ -26,5 +28,12 @@ class Order extends Model
     public function screening(): BelongsTo
     {
         return $this->belongsTo(Screening::class);
+    }
+
+    public function isCompleted(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['completed_at'] !== null,
+        );
     }
 }
