8209f91c53ebe8b986100f3a8d43090071ce0cae
Kostya <koooooostaaa@gmail.com>
2023-10-03 21:35:18 +0300

    create new policy

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class OrderPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, Order $order): Response
+    {
+        if (is_null($user)){
+            if ($order['session_id'] === request()->session()->getId()) {
+                return Response::allow();
+            }
+        }
+        
+        if ($order['user_id'] === $user['id']) {
+            return Response::allow();
+        }
+
+        return Response::deny('You are not authorised to do this');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Order $order): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Order $order): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Order $order): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Order $order): bool
+    {
+        return true;
+    }
+}
