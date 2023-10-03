f5e22d6a42ab7a35b3ce306c0712550d34a723f8
Kostya <koooooostaaa@gmail.com>
2023-10-03 21:47:35 +0300

    remove unnecessary methods, set limit for pagination in OrderController

--- app/Http/Controllers/Profile/OrderController.php
+++ app/Http/Controllers/Profile/OrderController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Order\ProfileOrderListResource;
 use App\Models\Order;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -27,58 +26,10 @@ class OrderController extends Controller
             ])
             ->withCount('seats')
             ->latest('created_at')
-            ->paginate();
+            ->paginate(8);
 
         return Inertia::render('Profile/Orders/Index', [
             'orders' => ProfileOrderListResource::collection($orders),
         ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Order $order)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Order $order)
-    {
-        //
     }
 }
