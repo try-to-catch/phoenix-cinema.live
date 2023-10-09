33ae1d98b04dc33297eae620b6686d98d443eb37
Kostya <koooooostaaa@gmail.com>
2023-10-09 17:51:27 +0300

    update form request

--- app/Http/Requests/Order/StoreOrderRequest.php
+++ app/Http/Requests/Order/StoreOrderRequest.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Requests\Order;
 
+use App\Rules\SeatAvailableRule;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreOrderRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = false;
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -18,17 +21,17 @@ class StoreOrderRequest extends FormRequest
         $currentYear = now()->year;
 
         return [
-            'seat_ids' => ['required', 'array'],
+            'seat_ids' => ['required', 'array', new SeatAvailableRule],
             'seat_ids.*' => ['required', 'string', 'exists:seats,id'],
             'card_data' => ['required', 'array'],
             'card_data.card_number' => ['required', 'string', 'size:16'],
             'card_data.expire_month' => ['required', 'integer', 'min:1', 'max:12',
                 function ($attribute, $value, $fail) use ($currentMonth, $currentYear) {
                     if ($value < $currentMonth && $this->input('card_data.expire_year') === $currentYear) {
-                        $fail($attribute.' has already passed for the current year.');
+                        $fail($attribute . ' has already passed for the current year.');
                     }
                 }],
-            'card_data.expire_year' => ['required', 'integer', 'min:'.$currentYear, 'max:'.$currentYear + 10],
+            'card_data.expire_year' => ['required', 'integer', 'min:' . $currentYear, 'max:' . $currentYear + 10],
             'card_data.cvv_code' => ['required', 'string'],
         ];
     }
