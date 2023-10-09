d63b41ace3268b1adceef9f231a1dfd2265bec26
Kostya <koooooostaaa@gmail.com>
2023-10-09 17:57:47 +0300

    create GetFutureMovies action

---
+++ app/Actions/Movies/GetFutureMoviesAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Actions\Movies;
+
+use App\Models\Movie;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class GetFutureMoviesAction
+{
+    public function handle(array $columns, ?int $count = null): Collection
+    {
+        return Cache::remember('movies:future', 60 * 60,
+            function () use ($columns, $count) {
+                $query = Movie::screeningSoonWithGenres()->orderBy('start_showing');
+
+                if (is_null($count)) {
+                    return $query->get($columns);
+                }
+
+                return $query->take($count)->get($columns);
+            });
+    }
+}
