999cc2260862862848be06b20f0b5435107f39e6
Kostya <koooooostaaa@gmail.com>
2023-10-09 17:48:44 +0300

    create SeatAvailable rule

---
+++ app/Rules/SeatAvailableRule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Seat;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class SeatAvailableRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param Closure(string): PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (Seat::whereIn('id', $value)->whereNull('order_id')->count() !== count($value)) {
+            $fail('Одне або кілька місць вже зайнято. Будь ласка, оновіть сторінку.');
+        }
+    }
+}
