a375800a53719ea420c7263dac868182d1c3c96c
Kostya <koooooostaaa@gmail.com>
2023-10-12 20:19:32 +0300

    update types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,23 @@
+import { RoleName } from '@/types/roles/RoleName'
+
 export interface User {
   id: number
   name: string
   email: string
   email_verified_at: string
+  roles: Role[]
+}
+
+export interface Role {
+  id: string
+  name: RoleName
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
   auth: {
     user: User
+  }
+  flash: {
+    message: string
   }
 }
