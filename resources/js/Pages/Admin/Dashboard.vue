85886ac20ff1c53151ebaf2561e2c17714d482a8
Kostya <koooooostaaa@gmail.com>
2023-10-12 20:12:06 +0300

    create Dashboard page

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Head>Dashboard</Head>
+  <AdminLayout>
+    <div class="py-12">
+      <div class="container">
+        <div class="xl:w-4/5 2xl:w-3/4 xl:mx-auto space-y-6">Dashboard</div>
+      </div>
+    </div>
+  </AdminLayout>
+</template>
+
+<style scoped></style>
