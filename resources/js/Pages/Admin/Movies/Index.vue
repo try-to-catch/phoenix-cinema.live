34b7ef589839f52fa8266d24c879b76d15e0a3e6
Kostya <koooooostaaa@gmail.com>
2023-10-16 19:53:55 +0300

    update Admin/Movie/Index page's ui

--- resources/js/Pages/Admin/Movies/Index.vue
+++ resources/js/Pages/Admin/Movies/Index.vue
@@ -1,29 +1,64 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3'
+import { Head, router } from '@inertiajs/vue3'
 import AdminLayout from '@/Layouts/AdminLayout.vue'
 import type { IPaginatedMovieCards } from '@/types/movies/IPaginatedMovieCards'
-import PaginatedMovieList from '@/Components/Movies/PaginatedMovieList.vue'
+import PaginatedTable from '@/Components/Tables/PaginatedTable.vue'
+import { computed, onMounted, ref, toRefs } from 'vue'
 
-const { movies } = defineProps<{
+const props = defineProps<{
   movies: Readonly<IPaginatedMovieCards>
 }>()
+
+const { movies } = toRefs(props)
+
+const formattedMovies = computed(() => {
+  return movies.value.data.map(movie => {
+    return {
+      ...movie,
+      genres: movie.genres.map(genre => genre.name).join(', '),
+    }
+  })
+})
+
+const searchInput = ref('')
+
+const filterProducts = () => {
+  const oldSearchInput = searchInput.value
+
+  setTimeout(() => {
+    if (searchInput.value === oldSearchInput) {
+      console.log('searchInput.value', oldSearchInput)
+      router.reload({
+        data: {
+          s: oldSearchInput,
+        },
+      })
+    }
+  }, 1250)
+}
+onMounted(() => {
+  const url = new URL(window.location.href)
+
+  searchInput.value = url.searchParams.get('s') ?? ''
+})
 </script>
 
 <template>
-  <Head>Фільми</Head>
+  <Head title="Фільми" />
   <AdminLayout>
     <div class="py-12">
       <div class="container">
-        <div class="xl:w-4/5 2xl:w-3/4 xl:mx-auto space-y-6">
+        <div class="space-y-6">
           <div class="p-4 sm:p-8 bg-tertiary border border-neutral-800 shadow sm:rounded-lg">
-            <h2 class="text-lg font-medium text-white">Фільми ({{ movies.meta.total }})</h2>
-
-            <PaginatedMovieList
-              :links="movies.meta.links"
-              :movies="movies.data"
-              class="mt-5"
-              card-size="sm"
-              route-name="admin.movies.show"
+            <h2 class="text-lg font-medium text-white pb-4">Фільми ({{ movies.meta.total }}){{ searchInput }}</h2>
+            <PaginatedTable
+              v-model:search-input="searchInput"
+              :items="formattedMovies"
+              route-item-key="slug"
+              route-name="admin.movies.edit"
+              :pagination-meta-links="movies.meta.links"
+              create-link-route-name="admin.movies.create"
+              @update:search-input="filterProducts"
             />
           </div>
         </div>
