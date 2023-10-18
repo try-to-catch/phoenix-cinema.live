c1a9953be3741a02b8c820386415d0dcc0609fe5
Kostya <koooooostaaa@gmail.com>
2023-10-18 22:06:09 +0300

    create a new table component

---
+++ resources/js/Components/Tables/PrimaryNavLink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+
+defineProps<{ routeName: string }>()
+</script>
+
+<template>
+  <Link
+    :href="route(routeName)"
+    class="sm:py-2 py-1 sm:text-sm text-xs text-white border border-neutral-800 rounded-lg sm:px-6 px-3 bg-primary text-center flex items-center justify-center hover:border-secondary duration-300"
+  >
+    <slot />
+  </Link>
+</template>
