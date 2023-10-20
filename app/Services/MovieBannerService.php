a8b6a3625d60f4b941280a1f0041f101226442b9
Kostya <koooooostaaa@gmail.com>
2023-10-20 20:41:22 +0300

    fix an issue(the params wasn't thrown in the function call)

--- app/Services/MovieBannerService.php
+++ app/Services/MovieBannerService.php
@@ -20,7 +20,7 @@ class MovieBannerService
      */
     public function create(Movie $movie, array $data, int $width = self::DEFAULT_WIDTH, int $height = self::DEFAULT_HEIGHT): Model
     {
-        $data['image'] = $this->imageService->store($data['image'], 'banners/movies');
+        $data['image'] = $this->imageService->store($data['image'], 'banners/movies', $width, $height);
 
         return $movie->banner()->create([
             'image' => $data['image'],
@@ -31,7 +31,7 @@ class MovieBannerService
 
     public function update(Movie $movie, array $data, int $width = self::DEFAULT_WIDTH, int $height = self::DEFAULT_HEIGHT): Model
     {
-        $data['image'] = $this->imageService->update($data['image'], $movie->banner->image);
+        $data['image'] = $this->imageService->update($data['image'], $movie->banner->image, $width, $height);
 
         return $movie->banner->update([
             'image' => $data['image'],
