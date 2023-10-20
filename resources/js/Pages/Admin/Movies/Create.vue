2d3ef257541abd4836a25457c75d75f7233bc0c5
Kostya <koooooostaaa@gmail.com>
2023-10-20 20:48:27 +0300

    update Admin/Movies/Create page with plenty of improvements (completed the page)

--- resources/js/Pages/Admin/Movies/Create.vue
+++ resources/js/Pages/Admin/Movies/Create.vue
@@ -11,6 +11,9 @@ import NumberInput from '@/Components/Breeze/NumberInput.vue'
 import DateInput from '@/Components/Forms/DateInput.vue'
 import FileInput from '@/Components/Forms/FileInput.vue'
 import TextInputWithSuggestions from '@/Components/Forms/TextInputWithSuggestions.vue'
+import Checkbox from '@/Components/Breeze/Checkbox.vue'
+import { ref, watch } from 'vue'
+import PrimaryButton from '@/Components/Breeze/PrimaryButton.vue'
 
 const { genres } = defineProps<{ genres: readonly IGenre[] }>()
 
@@ -34,6 +37,21 @@ const form = useForm<INewMovie>({
   banner: null,
   genres: [],
 })
+
+const submitForm = () => {
+  form.post(route('admin.movies.store'))
+}
+
+const hasBanner = ref(false)
+
+watch(hasBanner, value => {
+  if (value) {
+    form.banner = { fact: '', description: '', image: null }
+    return
+  }
+
+  form.banner = null
+})
 </script>
 
 <template>
@@ -48,8 +66,8 @@ const form = useForm<INewMovie>({
 
               <PrimaryNavLink route-name="admin.movies.index">Повернутися</PrimaryNavLink>
             </div>
-            <div class="">
-              <form class="mt-4 space-y-3" @submit.prevent>
+            <div>
+              <form class="mt-4 space-y-3" @submit.prevent="submitForm">
                 <div>
                   <TextInput id="title" v-model="form.title" label-inner="Назва" type="text" required />
                   <InputError :message="form.errors.title" class="mt-1.5" />
@@ -58,12 +76,13 @@ const form = useForm<INewMovie>({
                 <div>
                   <TextInputWithSuggestions
                     id="genres"
+                    v-model="form.genres"
                     label-inner="Жанри (відокрімлені пробілом)"
-                    required
                     :suggestions="genres"
                   />
                   <InputError :message="form.errors.genres" class="mt-1.5" />
                 </div>
+
                 <div>
                   <TextInput
                     id="main_cast"
@@ -76,7 +95,7 @@ const form = useForm<INewMovie>({
                 </div>
 
                 <div>
-                  <TextareaInput id="description" v-model="form.description" rows="2    " label-inner="Опис" required />
+                  <TextareaInput id="description" v-model="form.description" rows="2" label-inner="Опис" required />
                   <InputError :message="form.errors.description" class="mt-1.5" />
                 </div>
 
@@ -98,8 +117,8 @@ const form = useForm<INewMovie>({
                       v-model="form.duration_in_minutes"
                       label-inner="Тривалість"
                       required
-                      :max="600000000"
-                      :min="20"
+                      :max="500"
+                      :min="0"
                     />
                     <InputError :message="form.errors.duration_in_minutes" class="mt-1.5" />
                   </div>
@@ -126,7 +145,7 @@ const form = useForm<INewMovie>({
                       id="production_country"
                       v-model="form.production_country"
                       label-inner="Країна-виробник"
-                      autocomplete="county"
+                      autocomplete="production_country"
                       type="text"
                       required
                     />
@@ -142,7 +161,7 @@ const form = useForm<INewMovie>({
                       label-inner="Рік випуску"
                       required
                       :max="today.getFullYear()"
-                      :min="1900"
+                      :min="0"
                     />
                     <InputError :message="form.errors.release_year" class="mt-1.5" />
                   </div>
@@ -156,26 +175,70 @@ const form = useForm<INewMovie>({
                     <InputError :message="form.errors.original_title" class="mt-1.5" />
                   </div>
                   <div>
-                    <TextInput id="studio" v-model="form.studio" label-inner="Назва студії" type="text" />
+                    <TextInput
+                      id="studio"
+                      v-model="form.studio"
+                      label-inner="Назва студії"
+                      autocomplete="studio"
+                      type="text"
+                    />
                     <InputError :message="form.errors.studio" class="mt-1.5" />
                   </div>
                 </div>
 
                 <div class="grid sm:grid-cols-2 gap-3">
                   <div>
-                    <DateInput id="start_showing" v-model="form.start_showing" label-inner="Початок показу" />
+                    <DateInput id="start_showing" v-model="form.start_showing" required label-inner="Початок показу" />
                     <InputError :message="form.errors.start_showing" class="mt-1.5" />
                   </div>
                   <div>
-                    <DateInput id="end_showing" v-model="form.end_showing" label-inner="Закінчення показу" />
+                    <DateInput id="end_showing" v-model="form.end_showing" required label-inner="Закінчення показу" />
                     <InputError :message="form.errors.end_showing" class="mt-1.5" />
                   </div>
                 </div>
 
                 <div>
-                  <FileInput id="thumbnail" v-model="form.thumbnail" label-inner="Мініатрюра" required />
+                  <FileInput
+                    id="thumbnail"
+                    accept="image/*"
+                    label-inner="Мініатрюра"
+                    required
+                    @input="form.thumbnail = $event.target.files[0]"
+                  />
                   <InputError :message="form.errors.thumbnail" class="mt-1.5" />
                 </div>
+
+                <transition>
+                  <div v-if="form.banner && hasBanner" class="space-y-3">
+                    <TextInput id="banner_fact" v-model="form.banner.fact" label-inner="Цікавий факт" type="text" />
+
+                    <TextareaInput
+                      id="banner_description"
+                      v-model="form.banner.description"
+                      rows="2"
+                      label-inner="Опис банера"
+                      required
+                    />
+
+                    <FileInput
+                      id="banner_image"
+                      accept="image/*"
+                      label-inner="Банер"
+                      required
+                      @input="form.banner.image = $event.target.files[0]"
+                    />
+                    <InputError :message="form.errors.banner" class="mt-1.5" />
+                  </div>
+                </transition>
+
+                <div>
+                  <label class="flex items-center">
+                    <Checkbox v-model:checked="hasBanner" name="remember" />
+                    <span class="ml-2 text-sm text-neutral-400"> Має банер? (Тільки для фільмів A класу та вище) </span>
+                  </label>
+                </div>
+
+                <PrimaryButton type="submit"> Створити </PrimaryButton>
               </form>
             </div>
           </div>
@@ -185,4 +248,14 @@ const form = useForm<INewMovie>({
   </AdminLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
