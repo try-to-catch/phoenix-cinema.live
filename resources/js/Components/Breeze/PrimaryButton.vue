be08b2b1ef1a711d1d07d4685447ccd7a54b4602
Kostya <koooooostaaa@gmail.com>
2023-10-20 20:38:14 +0300

    define props(optional type) on PrimaryButton

--- resources/js/Components/Breeze/PrimaryButton.vue
+++ resources/js/Components/Breeze/PrimaryButton.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ type?: 'button' | 'submit' | 'reset' }>(), { type: 'button' })
+</script>
+
 <template>
   <button
+    :type="type"
     class="w-full text-white bg-secondary hover:bg-orange-550 duration-300 ease-in-out focus:ring-2 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm px-5 py-2.5"
   >
     <slot />
   </button>
 </template>
-<script setup lang="ts"></script>
