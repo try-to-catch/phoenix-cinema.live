0e7614562936efae4b20104ee35d43006f727a51
Kostya <koooooostaaa@gmail.com>
2023-11-05 19:44:43 +0200

    create new pages

---
+++ resources/js/Pages/Admin/MovieScreenings/Index.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { Head, router } from '@inertiajs/vue3'
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import { onMounted, ref, toRefs } from 'vue'
+import PaginatedTable from '@/Components/Tables/PaginatedTable.vue'
+import type { IPaginatedMovieScreeningsSummary } from '@/types/movieScreenings/IPaginatedMovieScreeningsSummary'
+
+const props = defineProps<{
+  movieScreenings: Readonly<IPaginatedMovieScreeningsSummary>
+}>()
+const { movieScreenings } = toRefs(props)
+
+const searchInput = ref('')
+
+const searchTemplates = () => {
+  const oldSearchInput = searchInput.value
+
+  setTimeout(() => {
+    if (searchInput.value === oldSearchInput) {
+      router.reload({
+        data: {
+          s: oldSearchInput,
+        },
+      })
+    }
+  }, 1250)
+}
+onMounted(() => {
+  const url = new URL(window.location.href)
+
+  searchInput.value = url.searchParams.get('s') ?? ''
+})
+</script>
+
+<template>
+  <Head title="Шаблони залів" />
+  <AdminLayout :is-wide="true">
+    <div class="py-12">
+      <div class="container">
+        <div class="space-y-6">
+          <div class="p-4 sm:p-8 bg-tertiary border border-neutral-800 shadow sm:rounded-lg">
+            <h2 class="text-lg font-medium text-white pb-4">Фільми ({{ movieScreenings.meta.total }})</h2>
+            <PaginatedTable
+              v-model:search-input="searchInput"
+              :items="movieScreenings.data"
+              route-item-key="slug"
+              show-route-name="admin.movie_screenings.show"
+              :pagination-meta-links="movieScreenings.meta.links"
+              create-link-route-name="admin.screenings.create"
+              @update:search-input="searchTemplates"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </AdminLayout>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Admin/MovieScreenings/Show.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import { Head } from '@inertiajs/vue3'
+import PrimaryNavLink from '@/Components/Tables/PrimaryNavLink.vue'
+import type { IOrganizedScreening } from '@/types/screenings/IOrganizedScreening'
+import ScreeningList from '@/Components/Screenings/ScreeningList.vue'
+
+const { screenings } = defineProps<{ screenings: IOrganizedScreening[] }>()
+</script>
+
+<template>
+  <Head title="Фільми" />
+  <AdminLayout :is-wide="false">
+    <div class="py-12">
+      <div class="container">
+        <div class="xl:w-4/5 2xl:w-3/4 xl:mx-auto space-y-6">
+          <div class="p-4 sm:p-8 bg-tertiary border border-neutral-800 shadow sm:rounded-lg">
+            <div class="flex justify-between items-center">
+              <h2 class="sm:text-lg text-base font-medium text-white">Список показів</h2>
+              <PrimaryNavLink route-name="admin.movie_screenings.index">Повернутися</PrimaryNavLink>
+            </div>
+            <section class="py-8 pb-6 sm:pb-12">
+              <div class="lg:container">
+                <ScreeningList v-if="screenings.length" :screenings="screenings" link-name="admin.screenings.show" />
+                <div v-else class="">
+                  <p class="text-white text-xl">Поки що показів не заплановано, слідкуйте за ононсами :3</p>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </AdminLayout>
+</template>
+
+<style scoped></style>
