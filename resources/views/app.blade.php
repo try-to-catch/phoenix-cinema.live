265142c9652b185659b5ba80743427e63ca7d296
Kostya <koooooostaaa@gmail.com>
2023-11-27 13:03:27 +0200

    Add meta description in the app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+        charset="Фенікс - популярна мережа кінотеатрів. Фенікс - це відмінний сервіс та задоволені клієнти. Обирайте та бронюйте квитки на понад 100 популярних фільмів прямо зараз">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+  <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+  <!-- Scripts -->
+  @routes
+  @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+  @inertiaHead
+</head>
+<body class="font-sans antialiased">
+@inertia
+</body>
 </html>
